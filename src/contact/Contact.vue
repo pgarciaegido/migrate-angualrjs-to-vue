6a06727027dd8df996ce506ac46696665c8956c3
Pablo García Egido <pgarciaegido@gmail.com>
2020-02-05 22:46:51 +0100

    stage6

---
+++ src/contact/Contact.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div class="contact">
+        <h1 class="content-text">Contact!!</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/routes/vueRoutes.js
+++ src/routes/vueRoutes.js
@@ -1,5 +1,7 @@
 import About from '../about/About.vue';
+import Contact from '../contact/Contact.vue';
 
 export default [
-    { name: 'about', path: '/about', component: About }
+    { name: 'about', path: '/about', component: About },
+    { name: 'contact', path: '/contact', component: Contact }
 ];
